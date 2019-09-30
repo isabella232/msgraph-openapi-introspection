d8237f0f2f54022115ad3a09b0f1084de81628fc
Darrel Miller <darrmi@microsoft.com>
2019-09-30 12:21:57 -0400

    Fixed link to swagger ui for none default versions

--- graphslice/Controllers/IndexController.cs
+++ graphslice/Controllers/IndexController.cs
@@ -21,13 +21,13 @@ namespace apislice.Controllers
         public async Task<IActionResult> Get(string graphVersion = "v1.0", bool forceRefresh = false)
         {
             var graphOpenApi = await OpenApiService.GetGraphOpenApiDocument(graphVersion,forceRefresh);
-            WriteIndex(this.Request.Scheme + "://"+ this.Request.Host.Value,graphOpenApi, Response.Body);
+            WriteIndex(this.Request.Scheme + "://"+ this.Request.Host.Value, graphVersion, graphOpenApi, Response.Body);
             
             return new EmptyResult();
         }
 
 
-        private static void WriteIndex(string baseUrl, OpenApiDocument graphOpenApi, Stream stream)
+        private static void WriteIndex(string baseUrl, string graphVersion, OpenApiDocument graphOpenApi, Stream stream)
         {
             var sw = new StreamWriter(stream);
             
@@ -38,18 +38,21 @@ namespace apislice.Controllers
 
             sw.AutoFlush = true;
 
+            sw.WriteLine("<head>");
+            sw.WriteLine("<link rel='stylesheet' href='./stylesheet.css' />");
+            sw.WriteLine("</head>");
             sw.WriteLine("<h1># OpenAPI Operations for Microsoft Graph</h1>");
             sw.WriteLine("<b/>");
             sw.WriteLine("<ul>");
             foreach (var item in indexSearch.Index)
             {
                 
-                var target = $"{baseUrl}/openapi?tags={item.Key.Name}&openApiVersion=3";
-                sw.WriteLine($"<li><a href='./openapi?tags={target}'>{item.Key.Name}</a>   [<a href='/swagger/index.html#url={target}'>Swagger UI</a>]</li>");
+                var target = $"{baseUrl}/openapi?tags={item.Key.Name}&openApiVersion=3&graphVersion={graphVersion}";
+                sw.WriteLine($"<li>{item.Key.Name} [<a href='./openapi?tags={target}'>OpenApi</a>]   [<a href='/swagger/index.html#url={target}'>Swagger UI</a>]</li>");
                 sw.WriteLine("<ul>");
                 foreach (var op in item.Value)
                 {
-                    sw.WriteLine("<li><a href='./openapi?operationIds=" + op.OperationId + "'>" + op.OperationId + "</a></li>");
+                    sw.WriteLine($"<li>{op.OperationId}  [<a href='./openapi?operationIds={op.OperationId}'>OpenAPI</a>]</li>");
                 }
                 sw.WriteLine("</ul>");
             }
